4703c73171996e2a2722014639190be4d46fc1aa
RiftNT <neilandreitorre02@gmail.com>
2023-12-28 10:31:29 +0800

    implemented swipe features

--- lib/src/screens/feed_screen.dart
+++ lib/src/screens/feed_screen.dart
@@ -1,70 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:meowz/src/models/cats_model.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class FeedScreen extends StatefulWidget {
-  const FeedScreen({super.key});
+  const FeedScreen({Key? key}) : super(key: key);
 
   @override
   State<FeedScreen> createState() => _FeedScreenState();
 }
 
 class _FeedScreenState extends State<FeedScreen> {
-  late Future _cat;
+  late Future<List<Cat>> _cats;
+  AppinioSwiperController _swiperController = AppinioSwiperController();
+  int _currentIndex = 0;
 
   @override
   void initState() {
-    _cat = Cat.fetchCat();
+    _cats = _fetchCats();
     super.initState();
+  }
+
+  Future<List<Cat>> _fetchCats() async {
+    final response = await http.get(Uri.parse(
+        'https://api.thecatapi.com/v1/images/search?has_breeds=1&limit=10&api_key=live_ww8GBeUHkAu51IVO2DmnEPwoPj8hOPVoetILTJHlSWIvWYx8H1r1W8ZJvtkLAwZR'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return List<Cat>.from(data.map((catData) => Cat.fromJson(catData)));
+    } else {
+      throw Exception('Failed to load cats: ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _cat,
+      future: _cats,
       builder: (ctx, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('${snapshot.hasError}'),
-            );
-          } else if (snapshot.hasData) {
-            var extractedData = snapshot.data as Cat;
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        } else if (snapshot.hasData) {
+          List<Cat> cats = snapshot.data as List<Cat>;
 
-            return Scaffold(
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text("Cat ID: ${extractedData.id}"),
-                    Image(image: NetworkImage(extractedData.url))
-                  ],
+          return Scaffold(
+            body: SizedBox.expand(
+              child: AppinioSwiper(
+                backgroundCardCount: 3,
+                swipeOptions: const SwipeOptions.symmetric(
+                  horizontal: true,
+                  vertical: false,
                 ),
+                maxAngle: 0,
+                controller: _swiperController,
+                onSwipeEnd: _swipeEnd,
+                onEnd: _onEnd,
+                cardCount: cats.length,
+                cardBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(16),
+                          child: Image.network(
+                            cats[index].url,
+                            fit: BoxFit.cover,
+                            height: double.infinity,
+                            width: double.infinity,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.cancel, size: 80, color: Colors.red),
+                                onPressed: () {
+                                  _swiperController.swipeLeft();
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.check_circle, size: 80, color: Colors.green),
+                                onPressed: () {
+                                  _swiperController.swipeRight();
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-            );
-          }
-        } else {
-          return const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                  'Please wait while data is loading..',
-                  style: TextStyle(fontSize: 14.0),
-                )
-              ],
             ),
           );
+        } else {
+          return const Center(
+            child: Text("No Data"),
+          );
         }
-        return const Center(
-          child: Text("No Data"),
-        );
       },
     );
+  }
+
+  void _swipeEnd(int previousIndex, int targetIndex, SwiperActivity activity) {
+    if (activity is Swipe) {
+      setState(() {
+        _currentIndex = targetIndex;
+      });
+    }
+  }
+
+  void _onEnd() {
+    _cats = _fetchCats();
   }
 }
